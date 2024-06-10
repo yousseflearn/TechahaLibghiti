346db38ed9b331731ea22a3839b9f3a571fa8bd3
yousseflearn <youssefeasylearn@gmail.com>
2024-06-10 17:53:29 -0400

    Create Admin Folder with Setup , Navbar , Sidebar, add page, list page and orders page

---
+++ admin/src/App.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Navbar from './components/Navbar/Navbar';
+import Sidebar from './components/Sidebar/Sidebar';
+import { Routes, Route } from 'react-router-dom';
+import Add from './pages/Add/Add.jsx';
+import List from './pages/List/List.jsx';
+import Orders from './pages/Orders/Orders.jsx';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      <div className="app-content">
+        <Sidebar />
+        <Routes>
+          <Route path="/add" element={<Add />} />
+          <Route path="/list" element={<List />} />
+          <Route path="/order" element={<Orders />} />
+        </Routes>
+      </div>
+    </div>
+  );
+};
+
+export default App;
