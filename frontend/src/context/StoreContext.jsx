a6fc5a25c74b7315ef1b552edb587d3d80e0a90d
yousseflearn <youssefeasylearn@gmail.com>
2024-06-20 09:35:51 -0400

    Add functionality to bring Data from database

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -1,12 +1,12 @@
 import { createContext, useEffect, useState } from 'react';
-import { food_list } from '../assets/assets';
-
+import axios from 'axios';
 export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = 'http://localhost:4000';
   const [token, setToken] = useState('');
+  const [food_list, setFoodList] = useState([]);
 
   const addToCart = (itemId) => {
     if (!cartItems[itemId]) {
@@ -31,10 +31,19 @@ const StoreContextProvider = (props) => {
     return totalAmount;
   };
 
+  const fetchFoodList = async () => {
+    const response = await axios.get(url + '/api/food/list');
+    setFoodList(response.data.data);
+  };
+
   useEffect(() => {
-    if (localStorage.getItem('token')) {
-      setToken(localStorage.getItem('token'));
+    async function loadData() {
+      await fetchFoodList();
+      if (localStorage.getItem('token')) {
+        setToken(localStorage.getItem('token'));
+      }
     }
+    loadData();
   }, []);
 
   const contextValue = {
